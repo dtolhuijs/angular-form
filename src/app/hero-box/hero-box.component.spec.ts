bf388fdb8f24d034362a1c70011f133030b485ed
Deisi Bonilla <deisi@growingminds.nl>
2021-03-25 17:37:44 +0100

    :sparkles: Feature: added hero box component

---
+++ src/app/hero-box/hero-box.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeroBoxComponent } from './hero-box.component';
+
+describe('HeroBoxComponent', () => {
+  let component: HeroBoxComponent;
+  let fixture: ComponentFixture<HeroBoxComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HeroBoxComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeroBoxComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/hero-box/hero-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-box',
+  templateUrl: './hero-box.component.html',
+  styleUrls: ['./hero-box.component.css']
+})
+export class HeroBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
